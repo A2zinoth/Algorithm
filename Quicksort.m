6b85a28b936edb7800afb6590d81462e2904f3c9
A2zinoth <caoguanghao@yeah.net>
2016-08-26 14:48:46 +0800

    Quicksort

---
+++ Quicksort.m
@@ -0,0 +1,32 @@
+//
+//  Quicksort.m
+//  oser
+//
+//  Created by mreport on 16/8/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];  // 枢轴 中位数 数组中第一个数
+    while (low < high) {
+        while (low < high && arr[high] >= pivot) high--;
+        arr[low] = arr[high];
+        while (low < high && arr[low] <= pivot) low++;
+        arr[high] = arr[low];
+    }
+    arr[low] = pivot;
+    return low;
+}
+
+int Quicksort(int arr[], int low, int high) {
+    int pivot;  // 下标
+    if(low < high) {
+        pivot = partition(arr, low, high);
+        Quicksort(arr, low, pivot-1);   // 数组 low~pivot-1 均比pivot小
+        Quicksort(arr, pivot+1, high);  // 数组 pivot+1~high 均比pivot大
+    }
+    return 0;
+}
