9f24fb4d7d621e6cdfca69d3029ab2454ff78c45
caoguanghao <caoguanghao@yeah.net>
2016-12-01 10:42:13 +0800

    add BinarySearch

---
+++ BinarySearch.m
@@ -0,0 +1,23 @@
+//
+//  BinarySearch.m
+//  oser
+//
+//  Created by A2zinoth on 2016/12/1.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int BinarySearch(int arr[], int low, int high, int target) {
+    int mid = 0;
+    while (low <= high) {
+        mid = low + ((high-low)>>1);    // 溢出
+        if (arr[mid] < target)
+            low = mid + 1;              // 常步数前进
+        else if (arr[mid] > target)
+            high = mid - 1;
+        else
+            return mid;
+    }
+    return -1;
+}
--- Quicksort.m
+++ Quicksort.m
@@ -1,15 +1,15 @@
 //
 //  Quicksort.m
-//  oser
+//  Algorithm
 //
-//  Created by mreport on 16/8/26.
+//  Created by A2zinoth on 16/8/26.
 //
 //
 
 #import <Foundation/Foundation.h>
 
 
-int partition(int arr[], int low, int high) {
+int Partition(int arr[], int low, int high) {
     int pivot = arr[low];  // 枢轴 中位数 数组中第一个数
     while (low < high) {
         while (low < high && arr[high] >= pivot) high--;
@@ -24,7 +24,7 @@ int partition(int arr[], int low, int high) {
 int Quicksort(int arr[], int low, int high) {
     int pivot;  // 下标
     if(low < high) {
-        pivot = partition(arr, low, high);
+        pivot = Partition(arr, low, high);
         Quicksort(arr, low, pivot-1);   // 数组 low~pivot-1 均比pivot小
         Quicksort(arr, pivot+1, high);  // 数组 pivot+1~high 均比pivot大
     }
