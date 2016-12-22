584384ad19f4a54be5fb8cf6d5e1c964172b0a8c
caoguanghao <caoguanghao@yeah.net>
2016-12-22 17:38:10 +0800

    Mergesort add Iteration and modify Recursion

--- Mergesort.m
+++ Mergesort.m
@@ -8,8 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-void Merge(int arr[], int temp[], int low, int mid, int high) {
+
+// temp size (high-low+1)
+void Merge(int arr[], int low, int mid, int high) {
     int i = low, j = mid+1, k = low;  //low~mid  mid+1~high
+    
+    int *temp = malloc(sizeof(int)*high-low+1);
     while (i != mid+1 && j != high+1) {
         if (arr[i] >= arr[j])
             temp[k++] = arr[j++];
@@ -22,14 +26,32 @@ void Merge(int arr[], int temp[], int low, int mid, int high) {
         temp[k++] = arr[j++];
     for (i = low; i <= high; i++)
         arr[i] = temp[i];
+    free(temp);
+}
+
+#pragma mark <Recursion>
+void Mergersort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        Mergersort(arr, low, mid);
+        Mergersort(arr, mid+1, high);
+        Merge(arr, low, mid, high);
+    }
 }
 
 
-void Mergersort(int arr[],int temp[], int low, int high) {
-    if (low < high) {
-        int mid = (low + high) / 2;
-        Mergersort(arr, temp, low, mid);
-        Mergersort(arr, temp, mid+1, high);
-        Merge(arr, temp, low, mid, high);
+#pragma mark <Iteration>
+int Min(int a, int b) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+void Mergersort_I(int arr[], int low, int high) {
+    for (int i = 1; i<(high-low+1); i=i<<1) {
+        for (int j = low; j <= high - i; j += (i<<1)) {
+            Merge(arr, j, i+j-1, Min((j+ (i<<1)-1), high) );
+        }
     }
 }
